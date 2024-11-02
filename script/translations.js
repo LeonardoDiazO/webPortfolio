0a35fe9afbe75e41e63824f0c274af92b2a45641
LeonardoDiazO <maximun_neo@hotmail.com>
2024-11-02 18:07:33 -0500

    feat: traduccion de ingles y español

---
+++ script/translations.js
@@ -0,0 +1,157 @@
+// translations.js
+
+const translations = {
+    es: {
+        menu: {
+            inicio: "INICIO",
+            sobremi: "SOBRE MI",
+            skills: "SKILLS",
+            curriculum: "CURRICULUM",
+            portfolio: "PORTFOLIO",
+            contacto: "CONTACTO",
+        },
+        inicio: {
+            nombre: "NICK PEREZ",
+            titulo: "Ingeniero de Software - Experto UI/UX",
+        },
+        sobremi: {
+            titulo: "Sobre Mí",
+            descripcion: "Hola, soy Nick Perez. Lorem ipsum dolor sit amet consectetur adipisicing elit...",
+            datosPersonales: {
+                cumpleaños: "Cumpleaños",
+                telefono: "Teléfono",
+                email: "Email",
+                website: "Website",
+                direccion: "Dirección",
+                cargo: "Cargo",
+                freelance: "FREELANCE",
+            },
+            intereses: {
+                juegos: "JUEGOS",
+                musica: "MÚSICA",
+                viajar: "VIAJAR",
+                mac: "MAC OS",
+                deporte: "DEPORTE",
+                libros: "LIBROS",
+                autos: "AUTOS",
+                fotos: "FOTOS",
+            },
+            botonCV: "Descargar CV",
+        },
+        skills: {
+            titulo: "Skills",
+            technicalSkills: "Technical Skills",
+            professionalSkills: "Professional Skills",
+            comunicacion: "Comunicación",
+            trabajoEquipo: "Trabajo en Equipo",
+            creatividad: "Creatividad",
+            dedicacion: "Dedicación",
+            gestionProyectos: "Proyect Management",
+        },
+        curriculum: {
+            titulo: "Curriculum",
+            educacion: "Educación",
+            experiencia: "Experiencia de trabajo",
+        },
+        portfolio: {
+            titulo: "PORTFOLIO",
+            proyectoTitulo: "Diseño Creativo",
+            proyectoDescripcion: "Fotografía",
+        },
+        contacto: {
+            titulo: "CONTACTO",
+            formulario: {
+                nombre: "Tú Nombre",
+                telefono: "Número telefónico",
+                correo: "Dirección de correo",
+                tema: "Tema",
+                mensaje: "Mensaje",
+                botonEnviar: "Enviar Mensaje",
+            },
+            ubicacion: "Nicaragua 159, San Rafael Mza",
+            llamar: "Llámanos",
+            email: "Email",
+        },
+        footer: {
+            subir: "Subir",
+        }
+    },
+
+    en: {
+        menu: {
+            inicio: "HOME",
+            sobremi: "ABOUT ME",
+            skills: "SKILLS",
+            curriculum: "RESUME",
+            portfolio: "PORTFOLIO",
+            contacto: "CONTACT",
+        },
+        inicio: {
+            nombre: "NICK PEREZ",
+            titulo: "Software Engineer - UI/UX Expert",
+        },
+        sobremi: {
+            titulo: "About Me",
+            descripcion: "Hi, I'm Nick Perez. Lorem ipsum dolor sit amet consectetur adipisicing elit...",
+            datosPersonales: {
+                cumpleaños: "Birthday",
+                telefono: "Phone",
+                email: "Email",
+                website: "Website",
+                direccion: "Address",
+                cargo: "Position",
+                freelance: "FREELANCE",
+            },
+            intereses: {
+                juegos: "GAMES",
+                musica: "MUSIC",
+                viajar: "TRAVEL",
+                mac: "MAC OS",
+                deporte: "SPORT",
+                libros: "BOOKS",
+                autos: "CARS",
+                fotos: "PHOTOS",
+            },
+            botonCV: "Download CV",
+        },
+        skills: {
+            titulo: "Skills",
+            technicalSkills: "Technical Skills",
+            professionalSkills: "Professional Skills",
+            comunicacion: "Communication",
+            trabajoEquipo: "Teamwork",
+            creatividad: "Creativity",
+            dedicacion: "Dedication",
+            gestionProyectos: "Project Management",
+        },
+        curriculum: {
+            titulo: "Resume",
+            educacion: "Education",
+            experiencia: "Work Experience",
+        },
+        portfolio: {
+            titulo: "PORTFOLIO",
+            proyectoTitulo: "Creative Design",
+            proyectoDescripcion: "Photography",
+        },
+        contacto: {
+            titulo: "CONTACT",
+            formulario: {
+                nombre: "Your Name",
+                telefono: "Phone Number",
+                correo: "Email Address",
+                tema: "Subject",
+                mensaje: "Message",
+                botonEnviar: "Send Message",
+            },
+            ubicacion: "Nicaragua 159, San Rafael Mza",
+            llamar: "Call Us",
+            email: "Email",
+        },
+        footer: {
+            subir: "Scroll Up",
+        }
+    },
+
+    // Se puede seguir agregando más idiomas
+};
